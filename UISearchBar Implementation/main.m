a554d334e5d088ba9d0067b91b538058fa3c7bea
Gagandeep Kaur <Gagan@Gagandeeps-MacBook-Pro.local>
2015-07-21 11:16:00 +0530

    Initial Commit

---
+++ UISearchBar Implementation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UISearchBar Implementation
+//
+//  Created by Gagandeep Kaur on 21/07/15.
+//  Copyright (c) 2015 Gagandeep Kaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
